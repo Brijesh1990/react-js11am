95bcd1798ed16802762e79f7d38e140eee1d0df9
Jaydeep-Ramanuj-tops <jaydeepramanuj.tops@gmail.com>
2025-06-13 12:28:17 +0530

    all data uloaded

---
+++ module-2-basic-javascript/js/calc.js
@@ -0,0 +1,31 @@
+// create an function for additions
+function ad() {
+  var a = parseInt(prompt('Enter a values :'));
+  var b = parseInt(prompt('Enter b values :'));
+  var res = a + b;
+  alert('Additions of numbers is :' + res);
+}
+
+// create an function for substractions
+function subs() {
+  var a = parseInt(prompt('Enter a values :'));
+  var b = parseInt(prompt('Enter b values :'));
+  var res = a - b;
+  alert('Substractions of numbers is :' + res);
+}
+
+// create an function for multiplications
+function mult() {
+  var a = parseInt(prompt('Enter a values :'));
+  var b = parseInt(prompt('Enter b values :'));
+  var res = a * b;
+  alert('Multiplications of numbers is :' + res);
+}
+
+// create an function for divsions
+function dv() {
+  var a = parseInt(prompt('Enter a values :'));
+  var b = parseInt(prompt('Enter b values :'));
+  var res = a / b;
+  alert('Divisions of numbers is :' + res);
+}
---
+++ module-3-advanced-javascript-ES6.0/javascript-function/address.js
@@ -0,0 +1,25 @@
+function add1() {
+  var ad = 'Patel shoes corner b/h alap green city shop-5 Rajkot-36005';
+  //   console.log(ad);
+  document.write(ad);
+}
+function add2() {
+  var ad =
+    'Patel shoes corner b/h telephone exchange saurastra kala kendra society  Rajkot-36005';
+  //   console.log(ad);
+  document.write(ad);
+}
+
+function add3() {
+  var ad =
+    'Patel shoes corner telephone exchange nakshatra-6 kendra society  Rajkot-36005';
+  //   console.log(ad);
+  document.write(ad);
+}
+
+function add4() {
+  var ad =
+    'Patel shoes corner b/h telephone exchange saurastra kala kendra society  Rajkot-36005';
+  //   console.log(ad);
+  document.write(ad);
+}
