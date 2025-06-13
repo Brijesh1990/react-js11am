5a0d4af5e25a677a4a121f9a4e19c453c0600195
Jaydeep-Ramanuj-tops <jaydeepramanuj.tops@gmail.com>
2025-06-13 12:22:33 +0530

    13-06-2025 all data uploaded

---
+++ module-3-advanced-javascript-ES6.0/interprete-javascript/interprete.js
@@ -0,0 +1,10 @@
+// abcd => binary language (010)
+// interpreter => interpreter is used to convert any machine level language to binary language.
+
+// interpreter is step by step check programme
+// javascript is a interprete based language
+
+var a = 10;
+var b = 20;
+var c = a + b;
+console.log('Additions of numbers is :' + c);
