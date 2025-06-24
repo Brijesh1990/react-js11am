7b6276e5281b89c09e746e8a24b9e74b5d43f8e3
Jaydeep-Ramanuj-tops <jaydeepramanuj.tops@gmail.com>
2025-06-24 11:37:44 +0530

    23-06-2025 all data uploaded

---
+++ module-3-advanced-javascript-ES6.0/oops/class.js
@@ -0,0 +1,17 @@
+// class A {
+//   constructor() {
+//     var name = 'Brijesh kumar pandey';
+//     console.log(name);
+//   }
+// }
+// var obj = new A();
+// console.log(obj);
+// console.log(typeof obj);
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+}
+var obj = new Animal('This is a Tiger');
+console.log(obj);
